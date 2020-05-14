532ecb205af5d09e683136f2d420c3eae8132741
xz <1187567532@qq.com>
2020-05-14 22:04:50 +0800

    nacos test

---
+++ src/main/java/com/xz/nacos/NacosApplication.java
@@ -0,0 +1,18 @@
+package com.xz.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xz
+ * @ClassName NacosApplication
+ * @Description
+ * @date 2020/5/11 0011 22:59
+ **/
+@SpringBootApplication
+public class NacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosApplication.class, args);
+    }
+}
---
+++ src/main/java/com/xz/nacos/service/NacosService.java
@@ -0,0 +1,11 @@
+package com.xz.nacos.service;
+
+/**
+ * @author xz
+ * @ClassName NacosService
+ * @Description
+ * @date 2020/5/11 0011 23:22
+ **/
+public interface NacosService {
+    void test();
+}
