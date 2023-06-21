415bc562c6abb7737777097e02030b96eebd7c4c
xz <1187567532@qq.com>
2023-06-21 11:27:29 +0800

    pom: test java spi(Processor)

--- nacos-provider/src/main/java/com/xz/nacos/CustomProcessor.java
+++ nacos-provider/src/main/java/com/xz/nacos/CustomProcessor.java
@@ -3,6 +3,8 @@ package com.xz.nacos;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.TypeElement;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Set;
 
 /**
@@ -10,8 +12,22 @@ import java.util.Set;
  * @date 2023/6/15 17:58
  */
 public class CustomProcessor extends AbstractProcessor {
-    @Override
+
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        return false;
+        try {
+            return this.processImpl(annotations, roundEnv);
+        } catch (Exception var5) {
+            StringWriter writer = new StringWriter();
+            var5.printStackTrace(new PrintWriter(writer));
+            return true;
+        }
+    }
+
+    private boolean processImpl(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        if (roundEnv.processingOver()) {
+
+        }
+
+        return true;
     }
 }
