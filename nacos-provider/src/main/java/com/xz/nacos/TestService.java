801ca240ea087a6bb9d60c94d3d8edcb2c0eb1c1
xz <1187567532@qq.com>
2023-06-26 15:01:25 +0800

    feat: flat map test

--- nacos-provider/src/main/java/com/xz/nacos/TestService.java
+++ nacos-provider/src/main/java/com/xz/nacos/TestService.java
@@ -1,5 +1,6 @@
 package com.xz.nacos;
 
+import com.google.common.collect.Lists;
 import com.xz.nacos.annotation.AutoService;
 import com.xz.nacos.annotation.CustomService;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -8,7 +9,8 @@ import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.TypeElement;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author xz
@@ -19,5 +21,27 @@ public class TestService extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         return false;
+    }
+
+    public static void main(String[] args) {
+        List<int[]> i = new ArrayList<>();
+        i.add(new int[]{1,3,5});
+        i.add(new int[]{2,4,6});
+        // stream流里面的元素也是集合类型时, 单纯的map要么类型不变 要么只能映射为某个单对象
+        List<Integer> collect = i.stream().flatMap(id -> {
+            List<Integer> i1 = new ArrayList<>();
+            for (int ii : id) {
+                i1.add(ii);
+            }
+            return i1.stream();
+        }).collect(Collectors.toList());
+        System.out.println(collect);
+
+//        List<String[]> i = new ArrayList<>();
+//        i.add(new String[]{"1","3","5"});
+//        i.add(new String[]{"2","4","6"});
+//
+//        List<String> collect = i.stream().flatMap(Arrays::stream).collect(Collectors.toList());
+
     }
 }
