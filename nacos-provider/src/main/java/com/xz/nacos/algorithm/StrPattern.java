c9dfafbd454787a66ef2988889f9e1588543cc0b
xiazhou <1187567532@qq.com>
2024-02-19 16:40:49 +0800

    feat: KMP 字符串匹配

---
+++ nacos-provider/src/main/java/com/xz/nacos/algorithm/StrPattern.java
@@ -0,0 +1,56 @@
+package com.xz.nacos.algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author xz
+ * @since 2024/2/19 10:04
+ */
+public class StrPattern {
+    public static void main(String[] args) {
+        String pattern = "abab";
+        /**
+         * abab
+         * 前缀： a ab aba
+         * 后缀： b ab bab
+         */
+        int[] help = new int[pattern.length()];
+        // 最大匹配长度
+        int maxLen = 0;
+        for (int i = 1; i < pattern.length(); i++) {
+            // 当不匹配时，回退到最近匹配的位置
+            while (maxLen > 0 && pattern.charAt(maxLen) != pattern.charAt(i)) {
+                maxLen = help[maxLen - 1];
+            }
+            // 计算匹配长度
+            if (pattern.charAt(maxLen) == pattern.charAt(i)) {
+                maxLen ++;
+            }
+            help[i] = maxLen;
+        }
+
+        System.out.println(matchCount("abcababcbabcabanbabab", help, pattern));
+    }
+
+    private static List<Integer> matchCount(String s, int[] help, String pattern) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int match = 0;
+        // 匹配时同理，利用匹配数组, 在失配时找到回退的位置
+        for (int i = 0; i < s.length(); i++) {
+            while (match > 0 && s.charAt(i) != pattern.charAt(match)) {
+                match = help[match - 1];
+            }
+            if (s.charAt(i) == pattern.charAt(match)) {
+                match ++;
+            }
+            if (match == pattern.length()) {
+                ans.add(i - match + 1);
+                match = help[match - 1];
+            }
+        }
+
+        return ans;
+    }
+}
